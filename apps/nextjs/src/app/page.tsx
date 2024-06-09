fb142c1955672d46b2199ca359a2770e9751d434
Winston Yeo <winstonyeo99@yahoo.com>
2024-06-09 10:23:47 +0800

    fix:  lint errors

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,7 +1,11 @@
-import { Editor } from '@winston/rte/editor';
+import { Editor } from "@winston/rte/editor";
 
 export const runtime = "edge";
 
 export default function HomePage() {
-	return <main className="container h-screen py-16"><Editor/></main>;
+	return (
+		<main className="container h-screen py-16">
+			<Editor />
+		</main>
+	);
 }
