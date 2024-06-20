9286e7c549e79bed44e9f0fc136561660df87d58
Winston Yeo <winstonyeo99@yahoo.com>
2024-06-20 18:44:53 +0800

    feat: new folder layout

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,13 +1,22 @@
 import { Editor } from "@winston/rte/editor";
 import { FileExplorer } from "@winston/rte/file-explorer";
-
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup,
+} from "@winston/ui/resizable";
 export const runtime = "edge";
 
 export default function HomePage() {
 	return (
-		<main className="container min-h-screen py-16">
-			<FileExplorer />
-			<Editor />
-		</main>
+		<ResizablePanelGroup direction="horizontal" className="min-h-screen">
+			<ResizablePanel defaultSize={200}>
+				<FileExplorer />
+			</ResizablePanel>
+			<ResizableHandle />
+			<ResizablePanel defaultSize={800} className="p-5">
+				<Editor />
+			</ResizablePanel>
+		</ResizablePanelGroup>
 	);
 }
